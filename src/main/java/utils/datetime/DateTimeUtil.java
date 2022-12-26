e6a0e8b9845c45f20e147d100c62f5ff49f9bf9e
LiAo <liao991224@gmail.com>
2022-12-26 09:44:16 +0800

    LiAo:DateTimeUtil 新增时间格式常量

--- src/main/java/utils/datetime/DateTimeUtil.java
+++ src/main/java/utils/datetime/DateTimeUtil.java
@@ -17,6 +17,14 @@ import java.util.Date;
  */
 public class DateTimeUtil {
 
+    public static final String YYYY = "yyyy";
+
+    public static final String YYYY_MM = "yyyy-MM";
+
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+
+    public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+
     private static final Logger logger = SkyeLogger.getLogger();
 
     /** 全局的格式化字符串,其存在默认值 */
